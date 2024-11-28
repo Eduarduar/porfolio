18d627cdc182a7acb13a93614791055343df7e5f
Eduarduar <eduardoarcega1997@gmail.com>
2024-11-28 11:55:12 -0600

    refactor: Hacer el campo de correo electrónico de solo lectura y ajustar el botón para enviar correo

--- src/components/HomePage/Email.tsx
+++ src/components/HomePage/Email.tsx
@@ -12,10 +12,12 @@ function Email() {
             type="text"
             className="bg-black/5 dark:bg-white/5 w-full py-1.5 px-3 rounded-lg grid place-items-center text-slate-600 dark:text-slate-300"
             value={userInfo.email}
+            readOnly
           />
           <div className="flex gap-2">
             <Tippy content="Send Email">
               <Button
+                as={'a'}
                 elevated
                 variant="soft-success"
                 href={`mailto:${userInfo.email}`}
